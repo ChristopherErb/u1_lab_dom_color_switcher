ce613abc2bde3e65658c5fb118ccabe2e8080715
Christopher Erb <christophererb01@gmail.com>
2023-05-12 13:37:39 -0400

    first and only commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
 // Write your DOM code here!
+const red1 = document.getElementById('redBtn')
+const white1 = document.querySelector("#whiteBtn")
+const blue1 = document.getElementById('blueBtn')
+const yellow1 = document.querySelector('#yellowBtn')
+
+
+red1.addEventListener('click', () => 
+{
+let body = document.querySelector('body')
+body.style.backgroundColor = 'red'
+})
+
+
+white1.addEventListener('click', () =>
+{
+    let body = document.querySelector('body')
+    body.style.backgroundColor = "white"
+})
+
+blue1.addEventListener('click', () =>
+{
+    let body = document.querySelector('body')
+    body.style.backgroundColor = "blue"
+})
+
+yellow1.addEventListener('click', () => {
+    let body = document.querySelector('body')
+    body.style.backgroundColor = "yellow"
+})
